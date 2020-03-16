b846d426db8844f7711330a0e864a74ecabed2f1
James Sheen <>
2020-03-16 10:24:29 +0000

    Updates

---
+++ precache-manifest.5f51227e3398cad6a23b7a224ea8ec0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030487177cf616ecc65fad6c5be24d3c",
+    "url": "/react-datepicker/index.html"
+  },
+  {
+    "revision": "ae3861d392d7d71c888e",
+    "url": "/react-datepicker/static/css/main.af75ad0c.chunk.css"
+  },
+  {
+    "revision": "0d74ab7e2c0776a7ac73",
+    "url": "/react-datepicker/static/js/2.a1b4dea2.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-datepicker/static/js/2.a1b4dea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3861d392d7d71c888e",
+    "url": "/react-datepicker/static/js/main.d6455e3f.chunk.js"
+  },
+  {
+    "revision": "9d408be18afdd9ef06de",
+    "url": "/react-datepicker/static/js/runtime-main.2977408e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2013db883069d0b690f870aefa648ce.js"
+  "/react-datepicker/precache-manifest.5f51227e3398cad6a23b7a224ea8ec0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-datepicker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
